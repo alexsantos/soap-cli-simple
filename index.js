53e9e538fcfed2ff0f011d8f8592d29fe45955b8
AlexSantos <santosam72@gmail.com>
2015-02-01 15:40:26 +0000

    no more template strings

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 'use strict';
 var request = require('request'),
-    gzip = require('gzip-simple'),
+    gzip = require('../gzip-simple'),
     xml2js = require('./utils/xml2js');
 
 function namespaces(ns) {
     var attributes = '';
     for (var name in ns) {
-        attributes += `${name}="${ns[name]}"`;
+        attributes += name + '="' + ns[name] + '" ';
     }
     return attributes.trim();
 }
 
 function envelope(operation, message, options) {
     var xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    xml += `<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" ${namespaces(options.namespaces)}>`;
+    xml += '<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
+        'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+        'xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" ' +
+        namespaces(options.namespaces) + '>';
     if (options.header) {
-        xml += `<env:Header>${options.header}</env:Header>`;
+        xml += '<env:Header>' + options.header + '</env:Header>';
     }
-    xml += `<env:Body>${xml2js.buildObject(message)}</env:Body>`;
+    xml += '<env:Body>' + xml2js.buildObject(message) + '</env:Body>';
     xml += '</env:Envelope>';
     console.log(xml);
     return xml;
